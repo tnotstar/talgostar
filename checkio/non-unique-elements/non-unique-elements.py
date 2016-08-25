57fc818233bdee41ab1535d3926b4accab10236d
Antonio Alvarado H <tnotstar@gmail.com>
2016-08-25 11:45:24 +0200

    added my first checkio's task solution
    - created a folder for checkio's tasks
    - added my first solved task `non-unique-elements`

---
+++ checkio/non-unique-elements/non-unique-elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def checkio(data):
+    return [_ for _ in data if data.count(_) > 1]
+
+if __name__ == "__main__":
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+    assert checkio([1, 2, 3, 4, 5]) == []
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+
+# EOF
