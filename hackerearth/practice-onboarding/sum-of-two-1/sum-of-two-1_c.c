04364aaf99a745367003cb38b8c7cbe9482a5daf
Antonio Alvarado Hernández <tnotstar@gmail.com>
2019-11-13 23:50:24 +0100

    added first solution to `sum-of-two-1`
    - created a folder structure for `hackerearth` problems
    - added solutions in `c` & `c++`, a README file and its Makefile

---
+++ hackerearth/practice-onboarding/sum-of-two-1/sum-of-two-1_c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;                  // Define two integer variables
+    scanf("%d %d", &num1, &num2);    // Get input for the two variables
+
+    // Write the logic to add these numbers here:
+    int sum = num1 + num2;
+    printf("%d\n", sum);
+
+    return 0;
+}
---
+++ hackerearth/practice-onboarding/sum-of-two-1/sum-of-two-1_cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;        // Define two integer variables
+    cin >> num1 >> num2;   // Get input for the two variables
+
+    // Write the logic to add these numbers here:
+    int sum = num1 + num2;
+    cout << sum << endl;   // Print the sum
+
+    return 0;
+}
